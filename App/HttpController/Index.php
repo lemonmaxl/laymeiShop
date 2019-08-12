132a69512be5908f1b85ac0fbd8b5f7d7709ee10
liule <doweit@163.com>
2019-08-12 16:14:58 +0800

    first init

---
+++ App/HttpController/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\HttpController;
+
+use EasySwoole\Http\AbstractInterface\Controller;
+
+/**
+ * 
+ */
+class Index extends Controller
+{
+	
+	public function index()
+	{
+		$this->response()->write("hello world");
+	}
+}
